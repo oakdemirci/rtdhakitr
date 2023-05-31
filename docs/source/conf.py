4b68d2f4ce0b78c94515356e7c1c4d69e99fd7ef
oakdemirci <oakdemirci@gmail.com>
2023-05-31 15:56:24 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Haki'
+copyright = '2023, oakdemirci'
+author = 'oakdemirci'
 
 release = '0.1'
 version = '0.1.0'
